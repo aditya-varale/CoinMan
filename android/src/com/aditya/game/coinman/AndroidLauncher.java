83c54eb1c3c693897b1eab257e12f06e99db9675
ADITYA <varalemcoe@gmail.com>
2021-04-20 18:37:09 +0530

    Initial Release

---
+++ android/src/com/aditya/game/coinman/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.aditya.game.coinman;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.aditya.game.coinman.CoinMan;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CoinMan(), config);
+	}
+}
